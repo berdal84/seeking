b339aaec880866af815a3f6b291ba6ce0a56df97
berdal84 <berenger@42borgata.com>
2023-11-25 00:33:04 -0500

    feat(JobTable): handle pagination

--- client/app/components/job-table.tsx
+++ client/app/components/job-table.tsx
@@ -2,42 +2,83 @@
 
 import useJobs from "@/app/hooks/useJobs";
 import {Spinner} from "@/app/components/spinner";
-import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import {Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow} from "@mui/material";
 import {schemas} from "@/app/typings/schemas";
+import {Button, TablePagination} from "@mui/base";
+import {ChangeEventHandler, MouseEvent, useState} from "react";
+import {Box} from "@mui/system";
 
 export default function JobTable() {
 
-  const { data, error, isLoading } = useJobs()
+  // todo: move this to a React.Context or Redux
+  const [ state, setState] = useState({page: 0, limit: 1})
+  const { data, isLoading, mutate } = useJobs(state)
 
   if ( isLoading || !data) {
     return <Spinner/>
   }
-  return <TableContainer component={Paper}>
-    <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-      <TableHead>
-        <TableRow>
-          <TableCell>Role</TableCell>
-          <TableCell align="right">Company</TableCell>
-          <TableCell align="right">URL</TableCell>
-          <TableCell align="right">Notes</TableCell>
-          <TableCell align="right">Events</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {data.item.map((job: schemas.Job) => (
-          <TableRow
-            key={job.id}
-          >
-            <TableCell component="th" scope="row">
-              {job.role}
+
+  function handleChangePage( event: MouseEvent<HTMLButtonElement> | null, page: number) {
+    console.log('handleChangePage', page)
+    setState({...state, page })
+  }
+
+  const handleChangeRowsPerPage: ChangeEventHandler<HTMLInputElement> = (event): void => {
+    const limit = Number(event.target.value)
+    console.log('handleChangeRowsPerPage', limit)
+    setState({...state, limit })
+  }
+
+  function handleRefresh() {
+    mutate()
+  }
+
+  return <Box sx={{ width: '100%' }}>
+    <Paper sx={{ width: '100%', mb: 2 }}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Role</TableCell>
+              <TableCell align="right">Company</TableCell>
+              <TableCell align="right">URL</TableCell>
+              <TableCell align="right">Notes</TableCell>
+              <TableCell align="right">Events</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.item.map((job: schemas.Job) => (
+              <TableRow
+                key={job.id}
+              >
+                <TableCell component="th" scope="row">
+                  {job.role}
+                </TableCell>
+                <TableCell align="right">{job.company}</TableCell>
+                <TableCell align="right">{job.url}</TableCell>
+                <TableCell align="right">{job.notes}</TableCell>
+                <TableCell align="right">{job.events.length} event(s)</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                colSpan={5}
+                rowsPerPageOptions={[1, 5, 10, 25]}
+                count={data.item_total_count}
+                rowsPerPage={state.limit}
+                page={state.page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+              />
+            </TableRow>
+            <TableCell>
+              <Button onClick={handleRefresh}>Refresh</Button>
             </TableCell>
-            <TableCell align="right">{job.company}</TableCell>
-            <TableCell align="right">{job.url}</TableCell>
-            <TableCell align="right">{job.notes}</TableCell>
-            <TableCell align="right">{job.events.length} event(s)</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  </TableContainer>
+          </TableFooter>
+        </Table>
+      </TableContainer>
+    </Paper>
+  </Box>
 }
--- client/app/hooks/useJobs.tsx
+++ client/app/hooks/useJobs.tsx
@@ -1,17 +1,19 @@
 import useSWR from "swr";
 import {schemas} from "@/app/typings/schemas";
 import api from '@/app/utilities/api'
+import {useEffect} from "react";
 
 const fetcher = ( input: NodeJS.fetch.RequestInfo,
                   init?: RequestInit,) => fetch(`${api.BASE_URL}${input}`, init).then(res => res.json())
 
-export default function useJobs(page = 0, limit = 10) {
+export default function useJobs({page = 0, limit = 10}) {
   const params = new URLSearchParams({page, limit})
-  const { data, error, isLoading } = useSWR<schemas.JobPage>(`/job/?${params.toString()}`, fetcher)
+  const { data, error, isLoading, mutate } = useSWR<schemas.JobPage>(`/job/?${params.toString()}`, fetcher )
 
   return {
     data,
     isLoading,
-    isError: error
+    error,
+    mutate
   }
 }
